d18112ff82a3c016fe45074b22eef3fa4c43ac1b
Abhay NJ <abhayjoshi029@gmail.com>
2024-01-30 10:40:12 +0530

    Added status check feature

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,7 +12,9 @@ fn main() {
     let connect = CustomMenuItem::new("connect".to_string(), "Connect");
     let disconnect = CustomMenuItem::new("disconnect".to_string(), "Disconnect");
     let hide = CustomMenuItem::new("visibility".to_string(), "Show");
+    let status = CustomMenuItem::new("status".to_string(), "Click to get status");
     let tray_menu = SystemTrayMenu::new()
+        .add_item(status)
         .add_item(connect)
         .add_item(disconnect)
         .add_native_item(SystemTrayMenuItem::Separator)
@@ -32,6 +34,9 @@ fn on_system_tray_event(app: &AppHandle, event: SystemTrayEvent) {
         SystemTrayEvent::MenuItemClick { id, .. } => {
             let item_handle = app.tray_handle().get_item(&id);
             match id.as_str() {
+                "status" => {
+                    item_handle.set_title(format!("Status: {:?}", warp_gui_app::status_check())).unwrap();
+                }
                 "visibility" => match unsafe { HIDE_TOGGLE } {
                     true => {
                         unsafe {
